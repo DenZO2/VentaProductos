8b9108135d5f1a911dcd6ff1481b286ed0f99cfd
Proudy <rodrigopelosi98@hotmail.com>
2024-09-18 20:04:31 -0300

    Modificacion

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace VentaProductos.Models;
 
 public class Cliente
 {
     public int Id { get; set; }
+
+    [StringLength(100, ErrorMessage = "El nombre debe contener entre (3) y (100) caracteres.", MinimumLength = 3)]
     public string? NombreCliente { get; set; }
     public string? ApellidoCliente { get; set; }
     public int Dni { get; set; }
